963fe5d3eec405245f8dedff8d757cc911c4e906
juga <juga@riseup.net>
2016-12-05 23:47:34 +0000

    move requirements to setup.py and metadata to __init__.py

--- dhcpcanon/__init__.py
+++ dhcpcanon/__init__.py
@@ -18,6 +18,43 @@
 # You should have received a copy of the GNU General Public License
 # along with dhcpcanon.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.1.4.1"
+try:
+    from _version import version
+except ImportError:
+    try:
+        from setuptools_scm import get_version
+        version = get_version()
+    except (ImportError, LookupError):
+        version = '0.1.6'
+
+__version__ = version
 __author__ = "juga"
 __author_mail__ = "juga@riseup.net"
+__description__ = "DHCP client disclosing less identifying information"
+__long_description__ = "Python implmentation of the DHCP Anonymity Profile \
+                        (RFC7844) designed for users that \
+                        wish to remain anonymous to the visited network \
+                        minimizing disclosure of identifying information."
+__website__ = 'https://github.com/juga0/dhcpcanon'
+__documentation__ = 'http://dhcpcanon.readthedocs.io/en/' + __version__
+__authors__ = []
+__copyright__ = """Copyright (C) 2016 <juga@riseup.net>
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+For details see the COPYRIGHT file distributed along this program."""
+
+__license__ = """
+    This package is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    any later version.
+
+    This package is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this package. If not, see <http://www.gnu.org/licenses/>.
+"""
--- setup.py
+++ setup.py
@@ -18,42 +18,36 @@
 #   along with dhcpcanon.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from setuptools import setup
-# To use a consistent encoding
-from os import path, listdir
-from pip.req import parse_requirements
-from dhcpcanon import __version__, __author__, __author_mail__
-
-DOC_DIR = 'docs'
-TEST_DIR = 'test'
-BIN_DIR = 'bin'
-
-here = path.abspath(path.dirname(__file__))
-install_reqs = parse_requirements(path.join(here, 'requirements.txt'),
-                                  session=False)
-reqs = [str(ir.req) for ir in install_reqs]
-sphinx_rst_files = [x for x in listdir(DOC_DIR) if x[-3:] == 'rst']
-sphinx_docs = [path.join(DOC_DIR, x) for x in sphinx_rst_files]
+from setuptools import setup, find_packages
+import dhcpcanon
 
 setup(
     name='dhcpcanon',
-    version=__version__,
-    description='DCHP IPv4 anonymity profile implementation (RFC7844).',
-    long_description="""DCHP client implementation of the anonymity profile (RFC7844)
-     using Scapy Automaton.
-     """,
-    author=__author__,
-    author_email=__author_mail__,
+    version=dhcpcanon.__version__,
+    description=dhcpcanon.__description__,
+    long_description=dhcpcanon.__long_description__,
+    author=dhcpcanon.__author__,
+    author_email=dhcpcanon.__author_mail__,
     license='GPLv3+',
-    url='https://github.com/juga0/dhcpcanon',
-    packages=['dhcpcanon'],
-    install_requires=reqs,
+    url=dhcpcanon.__website__,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    dependency_links=[
+        "https://pypi.python.org/simple/scapy==2.2.0-dev",
+        "https://pypi.python.org/simple/netaddr==0.7.10",
+        "https://pypi.python.org/simple/ipaddr==2.1.11",
+        "https://pypi.python.org/simple/pytz==2016.6.1",
+        "https://pypi.python.org/simple/pip==8.1.2",
+        "https://pypi.python.org/simple/pyroute2==0.4.11"
+    ],
     extras_require={
         'dev': ['ipython'],
         'test': ['coverage'],
     },
-    scripts=['bin/dhcpca.py'],
-    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'dhcpcanon = bin.dhcpca:main',
+        ]
+    },
     keywords='python scapy dhcp RFC7844 RFC2131 anonymity',
     classifiers=[
         'Development Status :: 3 - Alpha',
