64abd106f6bff8ad1ce8bd48917373904f495063
juga <juga@riseup.net>
2016-12-06 23:35:38 +0000

    minor version change

--- dhcpcanon/__init__.py
+++ dhcpcanon/__init__.py
@@ -25,7 +25,7 @@ except ImportError:
         from setuptools_scm import get_version
         version = get_version()
     except (ImportError, LookupError):
-        version = '0.1.8'
+        version = '0.1.8.1'
 
 __version__ = version
 __author__ = "juga"
