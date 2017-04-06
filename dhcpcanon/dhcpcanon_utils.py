b9f469729087f88117e42c98a004a8f501ccf182
juga <juga@riseup.net>
2017-04-06 21:48:15 +0000

    fix bug with package type in new version

--- dhcpcanon/dhcpcanon_utils.py
+++ dhcpcanon/dhcpcanon_utils.py
@@ -104,21 +104,21 @@ def now():
 
 
 def is_Offer(packet):
-    if DHCP in packet and DHCPTypes[packet[DHCP].options[0][1]] == 'offer':
+    if DHCP in packet and packet[DHCP].options[0][1] == 'offer':
         logger.debug('Packet is Offer.')
         return True
     return False
 
 
 def is_NAK(packet):
-    if DHCP in packet and DHCPTypes[packet[DHCP].options[0][1]] == 'nak':
+    if DHCP in packet and packet[DHCP].options[0][1] == 'nak':
         logger.debug('Packet is NAK.')
         return True
     return False
 
 
 def is_ACK(packet):
-    if DHCP in packet and DHCPTypes[packet[DHCP].options[0][1]] == 'ack':
+    if DHCP in packet and packet[DHCP].options[0][1] == 'ack':
         logger.debug('Packet is ACK.')
         return True
     return False
@@ -126,33 +126,46 @@ def is_ACK(packet):
 
 def parse_response(packet, debug=False):
     pkt_dict = {}
-    pkt_dict['server_ip'] = packet[IP].src
     pkt_dict['server_mac'] = packet[Ether].src
+    pkt_dict['server_ip'] = packet[IP].src
+    pkt_dict['server_port'] = packet[UDP].sport
     pkt_dict['client_ip_offered'] = packet[BOOTP].yiaddr
     logger.debug('offered ip %s', pkt_dict['client_ip_offered'])
     # pkt_dict['client_mac'] = str2mac(packet[BOOTP].chaddr)
     # pkt_dict['client_xid'] = packet[BOOTP].xid
 
-    for option in packet[DHCP].options:
-        if type(option) == tuple:
-            if option[0] == 'server_id':
-                pkt_dict['server_id'] = option[1]
-            if option[0] == 'subnet_mask':
-                pkt_dict['subnet_mask'] = option[1]
-            if option[0] == 'broadcast_address':
-                pkt_dict['broadcast_address'] = option[1]
-            if option[0] == 'router':
-                pkt_dict['router'] = option[1]
-            if option[0] == 'domain':
-                pkt_dict['domain'] = option[1]
-            if option[0] == 'name_server':
-                pkt_dict['name_server'] = option[1]
-            if option[0] == 'lease_time':
-                pkt_dict['lease_time'] = option[1]
-            if option[0] == 'renewal_time':
-                pkt_dict['renewal_time'] = option[1]
-            if option[0] == 'rebinding_time':
-                pkt_dict['rebinding_time'] = option[1]
+    from collections import OrderedDict
+    pkt_dict.update(packet[BOOTP].fields)
+    # {'giaddr': '0.0.0.0',
+    #  'op': 2,
+    #  'siaddr': '127.0.0.1',
+    #  'xid': 1234,
+    #  'yiaddr': '127.0.0.1'}
+    tuple_options_dict = OrderedDict([opt for opt in
+                                      packet[DHCP].options
+                                      if type(opt) is tuple])
+    pkt_dict.update(tuple_options_dict)
+    logger.debug('pkt_dict %s', pkt_dict)
+    # for option in packet[DHCP].options:
+    #     if type(option) == tuple:
+    #         if option[0] == 'server_id':
+    #             pkt_dict['server_id'] = option[1]
+    #         if option[0] == 'subnet_mask':
+    #             pkt_dict['subnet_mask'] = option[1]
+    #         if option[0] == 'broadcast_address':
+    #             pkt_dict['broadcast_address'] = option[1]
+    #         if option[0] == 'router':
+    #             pkt_dict['router'] = option[1]
+    #         if option[0] == 'domain':
+    #             pkt_dict['domain'] = option[1]
+    #         if option[0] == 'name_server':
+    #             pkt_dict['name_server'] = option[1]
+    #         if option[0] == 'lease_time':
+    #             pkt_dict['lease_time'] = option[1]
+    #         if option[0] == 'renewal_time':
+    #             pkt_dict['renewal_time'] = option[1]
+    #         if option[0] == 'rebinding_time':
+    #             pkt_dict['rebinding_time'] = option[1]
     return pkt_dict
 
 
