9007f97fef4aaf06a3c4ba140d49e32bdc120a3d
juga0 <juga@riseup.net>
2017-05-30 07:05:54 +0000

    check that scriptfile is not None

--- dhcpcanon/clientscript.py
+++ dhcpcanon/clientscript.py
@@ -21,44 +21,47 @@ class ClientScript(object):
     env = attr.ib(default=attr.Factory(dict))
 
     def script_init(self, lease, state, prefix='',  medium=''):
-        if type(state) == int:
-            reason = STATES2REASONS[state]
-        else:
-            reason = state
-        self.env['reason'] = reason
-        # FIXME: what is medium?
-        self.env['medium'] = medium
-        self.env['client'] = 'dhcpcanon'
-        self.env['pid'] = str(os.getpid())
-        self.env['interface'] = str(lease.interface)
-        self.env['ip_address'] = str(lease.address)
-        self.env['subnet_mask'] = lease.subnet_mask
-        self.env['network_number'] = str(lease.subnet)
-        self.env['broadcast_address'] = lease.broadcast_address
-        self.env['domain_name_servers'] = lease.name_server
-        self.env['routers'] = lease.router
-        self.env['dhcp_server_identifier'] = str(lease.server_id)
-        self.env['next_server'] = lease.next_server
-        self.env['domain_name'] = lease.domain
-        self.env['dhcp_lease_time'] = str(lease.lease_time)
-        self.env['dhcp_renewal_time'] = str(lease.renewal_time)
-        self.env['dhcp_rebinding_time'] = str(lease.rebinding_time)
-        self.env['expire'] = lease.expiry
-        self.env['renew'] = lease.renew
-        self.env['rebind'] = lease.rebind
-        # logger.debug('env %s', self.env)
+            if self.scriptname is not None:
+            if type(state) == int:
+                reason = STATES2REASONS[state]
+            else:
+                reason = state
+            self.env['reason'] = reason
+            # FIXME: what is medium?
+            self.env['medium'] = medium
+            self.env['client'] = 'dhcpcanon'
+            self.env['pid'] = str(os.getpid())
+            self.env['interface'] = str(lease.interface)
+            self.env['ip_address'] = str(lease.address)
+            self.env['subnet_mask'] = lease.subnet_mask
+            self.env['network_number'] = str(lease.subnet)
+            self.env['broadcast_address'] = lease.broadcast_address
+            self.env['domain_name_servers'] = lease.name_server
+            self.env['routers'] = lease.router
+            self.env['dhcp_server_identifier'] = str(lease.server_id)
+            self.env['next_server'] = lease.next_server
+            self.env['domain_name'] = lease.domain
+            self.env['dhcp_lease_time'] = str(lease.lease_time)
+            self.env['dhcp_renewal_time'] = str(lease.renewal_time)
+            self.env['dhcp_rebinding_time'] = str(lease.rebinding_time)
+            self.env['expire'] = lease.expiry
+            self.env['renew'] = lease.renew
+            self.env['rebind'] = lease.rebind
+            # logger.debug('env %s', self.env)
 
     def script_go(self, scriptname=None, env=None):
         scriptname = self.scriptname or scriptname
-        env = self.env or env
-        logger.debug('calling script %s with env %s', scriptname, env)
-        sp = subprocess.Popen([scriptname], stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE, env=env,
-                              close_fds=True, shell=False)
-        out, err = sp.communicate()
-        if sp.returncode != 0:
-            logger.debug('sp err %s', err)
-        return sp.returncode
+        if scriptname is not None:
+            env = self.env or env
+            logger.debug('calling script %s with env %s', scriptname, env)
+            sp = subprocess.Popen([scriptname], stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE, env=env,
+                                  close_fds=True, shell=False)
+            out, err = sp.communicate()
+            if sp.returncode != 0:
+                logger.debug('sp err %s', err)
+            return sp.returncode
+        return None
 
     def gen_lease_str(self):
         # FIXME: what are the numbers in renew, rebind, expire?
