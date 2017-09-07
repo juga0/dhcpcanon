a737f34e36b02836e62d7db1c018c4c43d1372b5
juga0 <juga@riseup.net>
2017-09-07 14:12:13 +0200

    Add possibility systemd service from setup.py
    * Add functions to detect path
    * Move systemd files to more suitable locations

--- setup.py
+++ setup.py
@@ -4,11 +4,48 @@
 # Copyright 2016, 2017 juga (juga at riseup dot net), MIT license.
 
 """Setup."""
+import subprocess
+
 from setuptools import find_packages, setup
 
 import dhcpcanon
 
 
+def systemd_unit_dir():
+    cmd = ['pkg-config',  '--variable', 'systemdsystemunitdir', 'systemd']
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    (stdout, stderr) = proc.communicate()
+    if proc.returncode != 0 or stdout is None:
+        return None  # systemd not found
+    if isinstance(stdout.strip(), bytes):
+        return stdout.strip().decode('utf-8')
+    return stdout.strip()
+
+
+def systemd_tmpfiles_dir():
+    # There doesn't seem to be a specific pkg-config variable for this
+    cmd = ['pkg-config',  '--variable', 'prefix', 'systemd']
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    (stdout, stderr) = proc.communicate()
+    if proc.returncode != 0 or stdout is None:
+        return None  # systemd not found
+    d = stdout.strip()
+    if isinstance(d, bytes):
+        d = d.decode('utf-8')
+    return d + '/lib/tmpfiles.d'
+
+
+if systemd_unit_dir():
+    data_files = [
+        (systemd_unit_dir(), ['systemd/dhcpcanon.service']),
+        (systemd_tmpfiles_dir(), ['tmpfiles.d/dhcpcanon.conf']),
+        ('sbin', ["sbin/dhcpcanon-script"])
+    ]
+else:
+    data_files = []
+print(data_files)
 setup(
     name='dhcpcanon',
     version=dhcpcanon.__version__,
@@ -37,6 +74,10 @@ setup(
             'dhcpcanon = dhcpcanon.dhcpcanon:main',
         ]
     },
+    # NOTE: not installing system files as the user might want to install them
+    # in a custom prefix or without systemd
+    # data_files=data_files,
+    zip_safe=False,
     include_package_data=True,
     keywords='python scapy dhcp RFC7844 RFC2131 anonymity',
     classifiers=[
@@ -48,6 +89,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-        "Topic :: System :: Networking",
+        'Topic :: System :: Networking',
     ],
 )
