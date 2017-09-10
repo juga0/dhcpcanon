c599c6009c706807d8f11ad1f867b0d0ccf3307a
juga0 <juga@riseup.net>
2017-09-10 22:20:15 +0200

    Bump version

--- dhcpcanon/__init__.py
+++ dhcpcanon/__init__.py
@@ -12,7 +12,7 @@ except ImportError:
         from setuptools_scm import get_version
         version = get_version()
     except (ImportError, LookupError):
-        version = '0.7.0'
+        version = '0.7.1'
 
 __version__ = version
 __author__ = "juga"
